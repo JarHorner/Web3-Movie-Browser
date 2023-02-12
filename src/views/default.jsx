3f6fdfd97cbe0a4b26f653a0e90f35d61b1524b6
Jarret Horner <jhorn384@mtroyal.ca>
2023-02-12 01:34:36 -0700

    added modal to About button. created state for movie details view

--- src/views/default.jsx
+++ src/views/default.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/header";
 import Filters from "../components/filters";
 
 const Default = (props) => {
+  const [defaultView, setDefaultView] = useState(true);
+
+  const renderDefaultView = () => {
+    setDefaultView(true);
+  };
+
+  const renderMovieDetailsView = () => {
+    setDefaultView(false);
+  };
+
   return (
     <div id="default_cntr" className="h-full flex flex-col">
       <Header renderHomeView={props.renderHomeView} />
       <div className="h-full flex flex-row">
-        <div
-          id="default_filters"
-          className="w-1/4 m-3 bg-slate-400 rounded-2xl"
-        >
-          <div className="flex flex-row justify-center items-center">
-            <h1 className="text-3xl font-bold">Filters</h1>
+        {defaultView ? (
+          <>
+            <div
+              id="default_filters"
+              className="w-1/4 m-3 bg-slate-400 rounded-2xl"
+            >
+              <div className="flex flex-row justify-center items-center">
+                <h1 className="text-3xl font-bold">Filters</h1>
+              </div>
+            </div>
+            <div
+              id="default_List"
+              className=" w-1/2 m-3 bg-slate-400 rounded-2xl"
+            >
+              <div className="flex flex-row justify-center items-center">
+                <h1 className="text-3xl font-bold">Movie List</h1>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div
+            id="default_filters"
+            className="w-5/6 m-3 bg-slate-400 rounded-2xl"
+          >
+            <div className="px-20 py-5 flex flex-row">
+              <h1 className="text-3xl font-bold">Movie Title</h1>
+            </div>
           </div>
-        </div>
-        <div id="default_List" className=" w-1/2 m-3 bg-slate-400 rounded-2xl">
-          <div className="flex flex-row justify-center items-center">
-            <h1 className="text-3xl font-bold">Movie List</h1>
-          </div>
-        </div>
+        )}
         <div
           id="default_Favorites"
           className="w-1/4 m-3 bg-slate-400 rounded-2xl"
