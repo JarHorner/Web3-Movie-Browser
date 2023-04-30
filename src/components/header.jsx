03b2bcfe3c8f70199f1231489d5579631addf2b8
Jarret Horner <JHorn384@mtroyal.ca>
2023-04-30 10:06:51 -0600

    changed location of about and icon in default page to boost navigation ease

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -23,10 +23,6 @@ const Header = (props) => {
       className="h-full bg-white border-b flex justify-start"
     >
       <div className="flex items-center ml-2 ">
-        <FilmIcon
-          className=" w-16 transition-all hover:cursor-pointer hover:scale-105 hover:bg-pink-700 hover:text-white hover:drop-shadow-2xl rounded-lg p-2 text-pink-700 mr-1 "
-          onClick={() => props.renderHomeView()}
-        />
         <AdjustmentsHorizontalIcon
           className={
             " w-14 transition-all hover:cursor-pointer hover:scale-105 hover:bg-pink-700 hover:text-white hover:drop-shadow-2xl rounded-lg p-2 ml-1   " +
@@ -40,16 +36,21 @@ const Header = (props) => {
         />
       </div>
       <div className="flex-1 flex items-center justify-center">
-        <h1 className="mx-2 text-3xl text-pink-900 font-bold">Movie Finder</h1>
+        <FilmIcon
+          className=" w-16 transition-all hover:cursor-pointer hover:scale-105 hover:bg-pink-700 hover:text-white hover:drop-shadow-2xl rounded-lg p-2 text-pink-700 mr-1 "
+          onClick={() => props.renderHomeView()}
+          title="Home"
+        />
+        <h1
+          className="mx-2 text-3xl text-pink-900 font-bold cursor-pointer"
+          title="About Us"
+          onClick={handleOpenModal}
+        >
+          Movie Browser
+        </h1>
       </div>
 
       <div className="flex justify-center items-center mr-2">
-        <button
-          className="py-2 px-4 text-white bg-pink-700 rounded-lg hover:scale-105 transition-all"
-          onClick={handleOpenModal}
-        >
-          About
-        </button>
         <HeartIcon
           className={
             " w-14 transition-all hover:cursor-pointer hover:scale-105 hover:bg-pink-700 hover:text-white hover:drop-shadow-2xl rounded-lg p-2  mx-2  " +
